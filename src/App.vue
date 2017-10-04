2ebaafe7406a57e89fe321e517d7a1ea19daa134
WillyLu326 <luzh0326@gmail.com>
2017-10-03 22:44:30 -0400

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div>
+    <app-user></app-user>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import User from './components/User.vue'
+  export default {
+    components: {
+      'app-user': User
+    }
+  }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
