0ff6fedc8c5c22185279167a4680bdac4840b173
WillyLu326 <luzh0326@gmail.com>
2017-10-03 22:54:53 -0400

    remove warning

--- src/components/Servers.vue
+++ src/components/Servers.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="col-xs-12 col-sm-6">
         <ul class="list-group">
-            <li class="list-group-item" v-for="server in 5">
-                Server #{{ server }}
-            </li>
+            <li class="list-group-item" v-bind:key="server" v-for="server in 5">
+                Server # {{ server }}
+            </li>   
         </ul>
     </div>
 </template>
