a5b6d93b4de9e17d300dbe8d3ecd9387d5212a73
WillyLu326 <luzh0326@gmail.com>
2017-10-04 20:41:43 -0400

    add editUser into MainUser

--- src/props/MainUser.vue
+++ src/props/MainUser.vue
@@ -4,7 +4,16 @@
           <div class="col-xs-12 col-sm-12">
               <h1>Main User Section</h1>
           </div>
-          
+            <app-edit-user></app-edit-user>
       </div>
   </div>
 </template>
+
+<script>
+    import EditUser from './EditUser.vue'
+    export default {
+        components: {
+            'app-edit-user': EditUser
+        }
+    }
+</script>
