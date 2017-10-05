64a08e422c2397e643431c9fe971ce6bc8e0403d
WillyLu326 <luzh0326@gmail.com>
2017-10-04 22:37:22 -0400

    implement AddUser.vue

---
+++ src/props/AddUser.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col-xs-12 col-sm-6 cmp">
+      
+  </div>
+</template>
+
+<script>
+    
+</script>
+
+<style scoped>
+    .cmp {
+        border: 1px solid blanchedalmond;
+        background-color: skyblue;
+    }
+</style>
