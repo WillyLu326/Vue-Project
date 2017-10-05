38e07dee110f627a9159e35c0a8c8996f3f32a02
WillyLu326 <luzh0326@gmail.com>
2017-10-04 22:10:55 -0400

    add form-group name

--- src/props/EditUser.vue
+++ src/props/EditUser.vue
@@ -1,19 +1,33 @@
 <template>
   <div class="col-xs-12 col-sm-6 cmp">
-      <h4>Edit User</h4>
-      <button class="btn btn-secondary">Update Name</button>
+    <h4>Edit User</h4>
+    <div class="row form-group">
+      <div class="col-sm-3">
+        <label for="name">Name: </label>
+      </div>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" name="name" v-model="name">
+      </div>
+    </div>
+
+    <button class="btn btn-success">Update Name</button>
   </div>
 </template>
 
 <script>
-    
+  export default {
+    props: {
+      name: String,
+      age: Number
+    }
+  }
 </script>
 
 <style scoped>
-    .cmp {
-        border: 1px solid pink;
-        padding: 10px;
-        background: orangered;
-    }
+.cmp {
+  border: 1px solid pink;
+  padding: 10px;
+  background: orangered;
+}
 </style>
 
