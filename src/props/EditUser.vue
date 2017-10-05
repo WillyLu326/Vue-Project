7561e29af25b27eebf249224718ad2f83e25b60e
WillyLu326 <luzh0326@gmail.com>
2017-10-04 20:40:00 -0400

    implement EditUser.vue

---
+++ src/props/EditUser.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="col-xs-12 col-sm-6 cmp">
+      <h4>Edit User</h4>
+      <button class="btn btn-secondary">Update Name</button>
+  </div>
+</template>
+
+<script>
+    
+</script>
+
+<style scoped>
+    .cmp {
+        border: 1px solid pink;
+        padding: 10px 5px;
+    }
+</style>
+
