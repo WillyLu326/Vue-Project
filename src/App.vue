d36c904136125ce9a0d3747be76d9fe3bc25e60c
WillyLu326 <luzh0326@gmail.com>
2017-10-04 21:50:30 -0400

    implement EditUser

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="container">
     <app-user></app-user>
+    <hr>
+    <app-main-user></app-main-user>
   </div>
 </template>
 
 <script>
   import User from './components/User.vue'
+  import MainUser from './props/MainUser.vue'
+
   export default {
     components: {
-      'app-user': User
+      'app-user': User,
+      'app-main-user': MainUser
     }
   }
 </script>
--- src/props/MainUser.vue
+++ src/props/MainUser.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="container">
-      <div class="row">
-          <div class="col-xs-12 col-sm-12">
-              <h1>Main User Section</h1>
-          </div>
-            <app-edit-user></app-edit-user>
+    <div class="row">
+      <div class="col-xs-12 col-sm-12">
+        <h1>Main User Section</h1>
       </div>
+      <app-edit-user></app-edit-user>
+    </div>
   </div>
 </template>
 
 <script>
-    import EditUser from './EditUser.vue'
-    export default {
-        components: {
-            'app-edit-user': EditUser
-        }
+  import EditUser from './EditUser.vue'
+
+  export default {
+    components: {
+      'app-edit-user': EditUser
     }
+  }
 </script>
