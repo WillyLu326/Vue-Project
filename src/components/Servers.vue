80c2bcc55ad1704f04504bb4bfbce828eeb3d578
WillyLu326 <luzh0326@gmail.com>
2017-10-05 22:57:47 -0400

    clean up code

--- src/components/Servers.vue
+++ src/components/Servers.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="col-xs-12 col-sm-6">
     <ul class="list-group">
-      <!-- <li class="list-group-item" v-bind:key="server" v-for="server in 5">
-        Server # {{ server }}
-      </li> -->
       <app-server v-for="(server, index) in servers" v-bind:key="server"
                 :server="server" :index="index" :selectedIndex="selectedIndex"
                 @click.native="goToServerDetail(server, index)">
