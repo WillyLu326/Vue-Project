8926605d57ca39404cd24093420fcc8ebfb7c2e5
WillyLu326 <luzh0326@gmail.com>
2017-10-05 23:10:29 -0400

    create EventBus.on function

--- src/components/ServerDetail.vue
+++ src/components/ServerDetail.vue
@@ -5,5 +5,20 @@
 </template>
 
 <script>
-  
+  import { EventBus } from './../main.js'
+
+  export default {
+    data() {
+      return {
+        status: null,
+        index: -1
+      }
+    },
+    created () {
+      this.EventBus.$on('server-status', obj => {
+        this.status = obj.status
+        this.index = obj.index
+      })
+    }
+  }
 </script>
