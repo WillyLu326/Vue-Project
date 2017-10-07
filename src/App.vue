b22bcc7a8f16ca3606a52cc03af4901a0e623064
WillyLu326 <luzh0326@gmail.com>
2017-10-06 22:21:06 -0400

    import Parent

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,21 @@
     <app-user></app-user>
     <hr>
     <app-main-user></app-main-user>
+    <hr>
+
   </div>
 </template>
 
 <script>
   import User from './components/User.vue'
   import MainUser from './props/MainUser.vue'
+  import Parent from './slot/Parent.vue'
 
   export default {
     components: {
       'app-user': User,
-      'app-main-user': MainUser
+      'app-main-user': MainUser,
+      'app-parent': Parent
     }
   }
 </script>
