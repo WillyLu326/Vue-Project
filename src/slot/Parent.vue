f670b32d8a61da72b5961cadf211f049df66890c
WillyLu326 <luzh0326@gmail.com>
2017-10-06 22:20:18 -0400

    add slot

--- src/slot/Parent.vue
+++ src/slot/Parent.vue
@@ -1,9 +1,19 @@
 <template>
-    
+  <div>
+    <h1>
+      <header>This is the header</header>
+    </h1>
+
+    <slot></slot>
+
+    <h5>
+      <footer>This is the footer</footer>
+    </h5>
+  </div>
 </template>
 
 <script>
-    
+
 </script>
 
 
