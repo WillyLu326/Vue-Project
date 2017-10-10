d6e0a49867d0e7b6fa4ca8d22a7458a73e85c503
WillyLu326 <luzh0326@gmail.com>
2017-10-09 21:44:28 -0400

    implement Quote.vue

---
+++ src/quotes/Quote.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+    <div class="quote">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style>
+  .quote {
+    margin: 5px 0px;
+    border: 1px solid black;
+    background: grey;
+    color: white;
+    cursor: pointer;
+  }
+  .quote:hover {
+    background: pink;
+    color: black;
+  }
+</style>
--- src/quotes/Quotes.vue
+++ src/quotes/Quotes.vue
@@ -1,3 +1,12 @@
 <template>
-  
+
 </template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
+
