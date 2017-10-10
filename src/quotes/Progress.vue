5a6152d3d80b91d59849e5369434f31fe5118dd1
WillyLu326 <luzh0326@gmail.com>
2017-10-09 22:33:21 -0400

    implement Progress.vue

--- src/quotes/Progress.vue
+++ src/quotes/Progress.vue
@@ -1,9 +1,17 @@
 <template>
-  
+  <div class="progress">
+    <div class="progress-bar" role="progressbar" 
+         aria-valuemin="0" aria-valuemax="100"
+         :style="{ width: curtQuotes / maxQuotes *100 + '%' }">
+      {{ curtQuotes }} / {{ maxQuotes }}
+    </div>
+  </div>
 </template>
 
 <script>
-  
+  export default {
+    props: ['curtQuotes', 'maxQuotes']
+  }
 </script>
 
 <style>
--- src/quotes/Quotes.vue
+++ src/quotes/Quotes.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <app-header @addQuote="addQuoteHandler"></app-header>
+    <app-progress :curtQuotes="quotes.length" :maxQuotes="maxQuotes"></app-progress>
+    <br>
     <app-quote-grid :quotes="quotes" :maxQuotes="maxQuotes"></app-quote-grid>
   </div>
 </template>
@@ -8,11 +10,13 @@
 <script>
   import QuoteGrid from './QuoteGrid.vue'
   import Header from './Header.vue'
+  import Progress from './Progress.vue'
 
   export default {
     components: {
       appQuoteGrid: QuoteGrid,
-      appHeader: Header
+      appHeader: Header,
+      appProgress: Progress
     },
     data () {
       return {
