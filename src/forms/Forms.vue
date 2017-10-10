fc8fad69f4762033da9232cce2c3dd4354a72514
WillyLu326 <luzh0326@gmail.com>
2017-10-09 22:59:00 -0400

    add email

--- src/forms/Forms.vue
+++ src/forms/Forms.vue
@@ -1,9 +1,12 @@
 <template>
   <form>
+      <h1>Form Vue</h1>
+      <hr>
       <div class="row">
-          <div class="col-sm-6 col-sm-offset-2">
-              <h1>Form Vue</h1>
-              <hr>
+          <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 
+                      col-lg-6 col-lg-offset-3 col-xl-6 col-xl-offset-3 form-group">
+              <label for="email">Email</label>
+              <input type="text" id="email" class="form-control">
           </div>
       </div>
   </form>
