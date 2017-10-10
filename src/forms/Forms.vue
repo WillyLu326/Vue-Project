e7159e951d861a52e3d01408db7eabdfac0ef270
WillyLu326 <luzh0326@gmail.com>
2017-10-09 22:54:37 -0400

    start form.vue

--- src/forms/Forms.vue
+++ src/forms/Forms.vue
@@ -1,5 +1,11 @@
 <template>
-  
+  <form>
+      <div class="row">
+          <div class="col-sm-6 col-sm-offset-2">
+              <h1>Form Vue</h1>
+          </div>
+      </div>
+  </form>
 </template>
 
 <script>
