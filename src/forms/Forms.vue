41e68104ead66acdd828381688278ffc96a486d1
WillyLu326 <luzh0326@gmail.com>
2017-10-09 23:04:43 -0400

    add password

--- src/forms/Forms.vue
+++ src/forms/Forms.vue
@@ -4,9 +4,16 @@
       <hr>
       <div class="row">
           <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 
-                      col-lg-6 col-lg-offset-3 col-xl-6 col-xl-offset-3 form-group">
+                      col-lg-6 col-lg-offset-3 col-xl-6 col-xl-offset-3">
+            <div class="form-group">
               <label for="email">Email</label>
               <input type="text" id="email" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" class="form-control">
+            </div>
           </div>
       </div>
   </form>
