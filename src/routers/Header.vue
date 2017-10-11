c7f5be7a55cdd7000c78ea5a5993985389e58ebf
WillyLu326 <luzh0326@gmail.com>
2017-10-10 21:22:13 -0400

    add exact into router-link

--- src/routers/Header.vue
+++ src/routers/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="nav nav-pills">
-     <router-link to="/" tag="li" active-class="active" class="nav-item nav-link"><a>Home</a></router-link>
+     <router-link to="/" tag="li" active-class="active" class="nav-item nav-link" exact><a>Home</a></router-link>
      <router-link to="/menu" tag="li" active-class="active" class="nav-item nav-link"><a>Menu</a></router-link>
   </div>
 </template>
